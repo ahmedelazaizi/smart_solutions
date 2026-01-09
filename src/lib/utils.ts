edacbb1d8c4bc6f17c7f8d9d32ba60309b82a559
Almostafa <ahmedsola27@gmail.com>
2026-01-10 01:13:44 +0200

    Refactor utils.ts to remove QueryClient and add cn function
    Removed QueryClient setup and added cn function for class name merging.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,6 @@
-import { QueryClient } from "@tanstack/react-query";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      staleTime: 60_000, // 1 minute
-      refetchOnWindowFocus: false,
-    },
-    mutations: {
-      retry: false,
-    },
-  },
-});
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
